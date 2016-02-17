8941742fcbe1380a67da6d0f464b9489bbd4533f
Arianna Benson <ariannabenson@college.harvard.edu>
2016-02-16 20:57:44 -0500

    more progress on prim!

--- prim.c
+++ prim.c
@@ -29,19 +29,53 @@ int main(int argc, char* argv[])
 	buildMinHeap(nodes);
 
 	// initialize all of our edges
+	float edges[5][5];
+	for (int i = 1; i < 5; i++)
+	{
+		for (int j = 1; j < 5; j++)
+		{
+			// ensure mirroring
+			edges[i][j] = 0.2 + j / 10 + i / 15;
+			edges[i][j] = 0.2 + j / 10 + i / 15; 
+		}
+	}
 
 	// while the queue is not empty, extract minimum node
 	while(nodes[0]->key != 0)
 	{
 		// get minimum node
 		node* u = extractMin(nodes);
-		int num = u->key;
+		// get number of node
+		int num = u->num;
 
 		// get edges in graph adjacent to u
+		for (int i = 1; i < 5; i++)
+		{
+			// get node i
+			node* nodeV = malloc(sizeof(node));
+			nodeV->num = i;
+			nodeV->key = 300;
+			nodeV->parent = NULL;
+			// get weight from node i to u
+			float weight = edges[num][i];
+			// get node i's key ????
+			float ikey = 3;
+			// see if node is in our existing queue ????
+			bool inQueue = true;
+			if (inQueue && weight < i)
+			{
+				
+			}
+		}
+	}
 
-		nodes[0]->key--;
-	} 
+	// find length of MST tree by SUMMING UP KID PARENT WEIGHTS!!!
+	int sum = 0;
+	for (int i = 1; i < 5; i++)
+	{
+		sum += nodes[i]->key;
+	}
 
 	// return the size of our MST
-	printf("Size: %i\n", 4);
+	printf("Length of MST: %i\n", sum);
 }
