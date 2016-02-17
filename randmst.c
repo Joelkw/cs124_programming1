06bbfce9e5b7db5703b2a7c574bd7abafe14871f
Joelkw <LoJoel3+gh@gmail.com>
2016-02-16 23:18:33 -0500

    compiles, some errors for large numbers, timer implemented, 2d 3d implemented

--- randmst.c
+++ randmst.c
@@ -60,6 +60,11 @@ float dist2d(float x1, float y1, float x2, float y2)
 	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 }
 
+float dist3d(float x1, float y1, float z1, float x2, float y2, float z2) 
+{
+	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+}
+
 int main( int argc, char* argv[] )
 {
 	// make sure format was correct
@@ -82,6 +87,9 @@ int main( int argc, char* argv[] )
 	// weight is a real chosen uniformly on [0,1]
 	if (dimension == 0)
 	{
+		// start timer
+		clock_t start = clock();
+		clock_t diff;
 		// build a graph stored as an adjacency matrix
 		// we know there is every possible edge so we just need points
 		float edgeWeights[numpoints][numpoints];	
@@ -96,35 +104,41 @@ int main( int argc, char* argv[] )
 
 		// THIS IS A FUNCTION FOR DEBUGGING
 		// IT PRINTS THE MATRIX
-		printf("edgeweight[1][0] %2.6f \r\n", edgeWeights[1][0]);
-		displayDim0(numpoints, edgeWeights);
+		// printf("edgeweight[1][0] %2.6f \r\n", edgeWeights[1][0]);
+		// displayDim0(numpoints, edgeWeights);
 
-		printf("graph dimension 0 is: \r\n");
-		printf("xxx ");
-		for (int k = 0; k < numpoints; k++ )
-		{
-			printf("%d:       ", k);
-		}
-		printf("\r\n");
-		for (int i = 0; i < numpoints; i++)
-		{
-			printf("%d:  ", i);
-			for (int j = 0; j < numpoints; j++)
-			{
-				printf("%2.6f ", edgeWeights[i][j]);
-			}
-			printf("\r\n");
-		}
+		// printf("graph dimension 0 is: \r\n");
+		// printf("xxx ");
+		// for (int k = 0; k < numpoints; k++ )
+		// {
+		// 	printf("%d:       ", k);
+		// }
+		// printf("\r\n");
+		// for (int i = 0; i < numpoints; i++)
+		// {
+		// 	printf("%d:  ", i);
+		// 	for (int j = 0; j < numpoints; j++)
+		// 	{
+		// 		printf("%2.6f ", edgeWeights[i][j]);
+		// 	}
+		// 	printf("\r\n");
+		// }
 		// END OF DEBUGGING FUNCTION
-		
+		diff = clock() - start;
+		int msec = diff * 1000 / CLOCKS_PER_SEC;
+		printf("Dim0 took %d seconds and %d milliseconds \n", msec/1000, msec%1000);
+
 	}
 
 	////////////////////////////////////////////////////
 	// Start of 2-d unit square of points implementation
-	//
 	////////////////////////////////////////////////////
 	if (dimension ==2) 
 	{
+		// start timer
+		clock_t start = clock();
+		clock_t diff;
+
 		// our graph is an array of heaps of edge distances and the
 		// connecting point
 		node* graph[numpoints];
@@ -167,38 +181,103 @@ int main( int argc, char* argv[] )
 			}
 		}
 
-		// // heuristic testing for now, some printfs 
-		// // check point gen
+		diff = clock() - start;
+		int msec = diff * 1000 / CLOCKS_PER_SEC;
+		printf("2D cube took %d seconds and %d milliseconds \n", msec/1000, msec%1000);
+
+		/////////////////////////////////////////////
+		// Start of various testing for 2d 
+		/////////////////////////////////////////////
+		// // * heuristic testing for now, some printfs 
+		// // * check point gen
 		// for (int i = 0; i < numpoints; i++) 
 		// {
 		// 	printf("point %d was (%2.6f,%2.6f) \r\n", i, xs[i],ys[i]);
 		// }
-		// // check distance
+		// // * check distance
 		// printf("dist between points 0 and 1 was: %2.6f \r\n", dist2d(xs[0],ys[0],xs[1],ys[1]));
- 		// // check LL implementation
- 		for (int i = 0; i < numpoints; i++)
- 		{
- 			
- 				printf("%d point's first point was edge: %2.6f and vertex: %d \r\n", i, graph[i]->dist, graph[i]->nodeIndex);
- 			
- 		}	
+ 		// // * check LL implementation
+ 		// for (int i = 0; i < numpoints; i++)
+ 		// {
+ 		// 	 printf("%d point's first point was edge: %2.6f and vertex: %d \r\n", i, graph[i]->dist, graph[i]->nodeIndex);
+ 		// }	
+	}
+
+	////////////////////////////////////////////////////
+	// Start of 3-d unit square of points implementation
+	////////////////////////////////////////////////////
+	if (dimension == 3) 
+	{
+		// start timer
+		clock_t start = clock();
+		clock_t diff;
+
+		// our graph is an array of heaps of edge distances and the
+		// connecting point
+		node* graph[numpoints];
+
+		// a place for our x and y coords
+		float xs[numpoints];
+		float ys[numpoints];
+		float zs[numpoints];
+		// generate our points
+		for (int i = 0; i < numpoints; i++) 
+		{
+			xs[i] = randNum();
+			ys[i] = randNum();
+			zs[i] = randNum();
+			// initialize our graph here
+			node* nullHead = NULL;
+			graph[i] = nullHead;
+		}
+		// instert points into graph
+		// - i is the node we are on
+		for (int i = 0; i < numpoints; i++)
+		{
+			// - j wil loop through other nodes, don't care about i to i
+			for (int j = 0; (j < numpoints); j++)
+			{
+				if (i != j)
+				{
+					// get the head of the list
+					//node* head = graph[i];
+					// if it is the first point so far
+					//printf("i: %d j: %d \r\n", i, j);
+					node* newEdge = malloc(sizeof(node));
+					if (newEdge == NULL)
+					{
+						exit(1);
+					}
+					newEdge->dist = dist3d(xs[i],ys[i],zs[i],xs[j],ys[j],zs[j]);
+					newEdge->nodeIndex = j;
+					newEdge->next = graph[i];
+					graph[i] = newEdge;
+				}
+			}
+		}
+
+		diff = clock() - start;
+		int msec = diff * 1000 / CLOCKS_PER_SEC;
+		printf("3D cube took %d seconds and %d milliseconds \n", msec/1000, msec%1000);
+
+		/////////////////////////////////////////////
+		// Start of various testing for 3d 
+		/////////////////////////////////////////////
+		// // * heuristic testing for now, some printfs 
+		// * check point gen
+		// for (int i = 0; i < numpoints; i++) 
+		// {
+		// 	printf("point %d was (%2.6f, %2.6f, %2.6f) \r\n", i, xs[i],ys[i],zs[i]);
+		// }
+		// // * check distance
+		// printf("dist between points 0 and 1 was: %2.6f \r\n", dist2d(xs[0],ys[0],xs[1],ys[1]));
+ 		// // * check LL implementation
+ 		// for (int i = 0; i < numpoints; i++)
+ 		// {
+ 		// 	 printf("%d edge: %2.6f vertex: %d | ", i, graph[i]->dist, graph[i]->nodeIndex);
+ 		// }	
 	}
 
 
-
-
-	// returns random number b/w [0,1]
-
-	// for (int i = 0; i < 20; i++) 
-	// {
-	// 	//printf("random was %2.6f\r\n", randNum());
-	// }
-	// for (int i = 0; i < argc; i++)
-	// {
-	// 	 printf("argv %i was %s", i, argv[i]);
-	// }
-
-	// graphs on n vertices where each edge is a real
-	// number chosen randomly on [0,1]
 
 }
