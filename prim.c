a70fcb9d75b0f1d93ac073a5ae5d103e4903cd10
Arianna Benson <ariannabenson@college.harvard.edu>
2016-02-16 22:28:48 -0500

    factor out prim

--- prim.c
+++ prim.c
@@ -8,48 +8,10 @@
 
 #include "header.h"
 
-int main(int argc, char* argv[])
+float prim(node* nodes[5], float edges[5][5])
 {
-	// initialize all of our nodes
-	node* nodes[5]; 
-	for (int i = 0; i <= 4; i++)
-	{
-		nodes[i] = malloc(sizeof(node));
-		nodes[i]->num = i;
-		nodes[i]->key = 10 + i;
-		nodes[i]->parent = NULL;
-	}
-
-	// let the 0th node be the size of the heap
-	nodes[0]->key = 4;
-	// set the root's key to 0
-	nodes[1]->key = 0;
-
 	// heapify our nodes
 	buildMinHeap(nodes);
-
-	// initialize all of our edges
-	float edges[5][5];
-	for (int i = 1; i < 5; i++)
-	{
-		for (int j = 1; j < 5; j++)
-		{
-			// ensure mirroring
-			edges[i][j] = 0.2 + (float) j / 10 + (float) i / 3;
-			edges[j][i] = edges[i][j]; 
-		}
-	}
-
-	// print out edges (for fun)
-	for (int i = 1; i < 5; i++)
-	{
-		for (int j = 1; j < 5; j++)
-		{
-			// ensure mirroring
-			printf("%f ", edges[i][j]); 
-		}
-		printf("\n");
-	}
 
 	// create a forest to store our path
 	node* forest[4];
@@ -92,4 +54,48 @@ int main(int argc, char* argv[])
 
 	// return the size of our MST
 	printf("Length of MST: %f\n", sum);
+	return sum;
+}
+
+int main(int argc, char* argv[])
+{
+	// initialize all of our nodes
+	node* nodes[5]; 
+	for (int i = 0; i <= 4; i++)
+	{
+		nodes[i] = malloc(sizeof(node));
+		nodes[i]->num = i;
+		nodes[i]->key = 10 + i;
+		nodes[i]->parent = NULL;
+	}
+
+	// let the 0th node be the size of the heap
+	nodes[0]->key = 4;
+	// set the root's key to 0
+	nodes[1]->key = 0;
+
+	// initialize all of our edges
+	float edges[5][5];
+	for (int i = 1; i < 5; i++)
+	{
+		for (int j = 1; j < 5; j++)
+		{
+			// ensure mirroring
+			edges[i][j] = 0.2 + (float) j / 10 + (float) i / 3;
+			edges[j][i] = edges[i][j]; 
+		}
+	}
+
+	// print out edges (for fun)
+	for (int i = 1; i < 5; i++)
+	{
+		for (int j = 1; j < 5; j++)
+		{
+			// ensure mirroring
+			printf("%f ", edges[i][j]); 
+		}
+		printf("\n");
+	}
+
+	float x = prim(nodes, edges);
 }
